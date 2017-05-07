8d1a74ba8ad95af556705e754ae7c12f4ccb816c
Michal Stawinski <michal.stawinski@gmail.com>
2017-05-07 22:32:47 +0200

    Switched to mesh example

--- blinky/blinky.cpp
+++ blinky/blinky.cpp
@@ -1,17 +1,77 @@
 #include <Arduino.h>
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMesh.h>
+
+unsigned int request_i = 0;
+unsigned int response_i = 0;
+
+String manageRequest(String request);
+
+/* Create the mesh node object */
+ESP8266WiFiMesh mesh_node = ESP8266WiFiMesh(ESP.getChipId(), manageRequest);
+
+
+void blink(int blinksNumber) {
+	for (int i = 0; i < blinksNumber; i++) {
+		digitalWrite(LED_BUILTIN, LOW);
+		delay(30);
+		digitalWrite(LED_BUILTIN, HIGH);
+
+		if (i == blinksNumber - 1)
+			break;
+
+		delay(100);
+	}
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(50);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(50);                       // wait for a second
+/**
+ * Callback for when other nodes send you data
+ *
+ * @request The string received from another node in the mesh
+ * @returns The string to send back to the other node
+ */
+String manageRequest(String request)
+{
+	/* Print out received message */
+	Serial.print("received: ");
+	Serial.println(request);
+
+	/* return a string to send back */
+	char response[60];
+	sprintf(response, "Hello world response #%d from Mesh_Node%d.", response_i++, ESP.getChipId());
+
+	blink(1);
+
+	return response;
 }
 
+void setup()
+{
+	Serial.begin(115200);
+	delay(10);
 
+	pinMode(LED_BUILTIN, OUTPUT);
+
+	Serial.println();
+	Serial.println();
+	Serial.printf("Setting up mesh node... me=%d\n", ESP.getChipId());
+
+	/* Initialise the mesh node */
+	mesh_node.begin();
+}
+
+void loop()
+{
+	/* Accept any incoming connections */
+	mesh_node.acceptRequest();
+
+	/* Scan for other nodes and send them a message */
+	char request[60];
+	sprintf(request, "Hello world request #%d from Mesh_Node%d.", request_i++, ESP.getChipId());
+
+	blink(2);
+
+	mesh_node.attemptScan(request);
+	delay(1000);
+}
